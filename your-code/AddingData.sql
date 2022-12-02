bab547ac7b629c2d648264bf9abdae4b860bcfa9
JorgeLlorente <jorgellorente1994@gmail.com>
2022-12-02 22:56:08 +0100

    Lab Mysql

---
+++ your-code/AddingData.sql
@@ -0,0 +1,33 @@
+USE mydb
+
+INSERT INTO coche
+
+VALUES 
+(1,"Volkswagen" , "Tiguan", 2019, "Blue"),
+(2, "Peugeot", "Rifter", 2019, "Red"),
+(3, "Ford", "Fusion", 2018, "White");
+
+INSERT INTO cliente
+
+VALUES 
+(1, "Pablo Picasso", 18127127, "-", 
+"Paseo de la Chopera, 14", "Madrid", "Madrid", "Spain", 28045);
+
+INSERT INTO vendedor
+
+VALUES 
+(1, "Petey Cruiser", "Madrid"),
+(2, "Anna Sthesia", "Barcelona"),
+(3, "Paul Molive", "Berlin");
+
+INSERT INTO fecha
+
+VALUES
+(1, "2022-09-21"),
+(2, "2022-09-22"),
+(3, "2022-09-23");
+
+INSERT INTO facturas
+
+VALUES 
+(1, 1, 2, 3, 1)
