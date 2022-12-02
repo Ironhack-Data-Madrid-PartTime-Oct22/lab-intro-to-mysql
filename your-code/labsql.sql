331fa3163e198a3ddd2d4f855261dd102ef648d7
ppastorcarrillo <116591098+ppastorcarrillo@users.noreply.github.com>
2022-12-02 14:31:49 +0100

    cars completo, problemas al añadir el phone number en la tabla de customer

---
+++ your-code/labsql.sql
@@ -0,0 +1,10 @@
+SELECT * FROM labsql.customer;
+USE labsql;
+
+INSERT INTO customer
+VALUES 
+(0, "10001", "Pablo Picasso", "+34 636176382", "eluno@gamil.com", "Paseo de la Chopera, 14", "Madrid", "Madrid", "Spain", "28045"),
+(1, "20001", "Abraham Lincoln", "+1 3059077086","eldos@hotmail.com", "120 SW 8th St ", "Miami", "Florida", "United States", "33130"),
+(2, "30001", "Napoléon Bonaparte", "+33 179754000","eltreclaro@yahoo.com", "40 Rue du Colisée", "Paris", "Île-de-France", "France", "75008");
+
+
