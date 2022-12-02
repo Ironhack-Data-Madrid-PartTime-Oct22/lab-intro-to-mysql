40463c9c491b4e24f4317490aea1d9d4c634969c
ppastorcarrillo <116591098+ppastorcarrillo@users.noreply.github.com>
2022-12-02 14:40:16 +0100

    tambien error en la tabla de customers 🫠

--- your-code/labsql.sql
+++ your-code/labsql.sql
@@ -1,10 +1,13 @@
-SELECT * FROM labsql.customer;
+SELECT * FROM labsql.salesperson;
 USE labsql;
 
-INSERT INTO customer
-VALUES 
-(0, "10001", "Pablo Picasso", "+34 636176382", "eluno@gamil.com", "Paseo de la Chopera, 14", "Madrid", "Madrid", "Spain", "28045"),
-(1, "20001", "Abraham Lincoln", "+1 3059077086","eldos@hotmail.com", "120 SW 8th St ", "Miami", "Florida", "United States", "33130"),
-(2, "30001", "Napoléon Bonaparte", "+33 179754000","eltreclaro@yahoo.com", "40 Rue du Colisée", "Paris", "Île-de-France", "France", "75008");
-
-
+INSERT INTO
+VALUES
+(0, "00001", "Petey Cruiser", "Madrid"),
+(1, "00002", "Anna Sthesia", "Barcelona"),
+(2, "00003", "Paul Molive", "Berlin"),
+(3, "00004", "Gail Forcewind", "Paris"),
+(4, "00005", "Paige Turner", "Mimia"),
+(5, "00006", "Bob Frapples", "Mexico City"),
+(6, "00007", "Walter Melon", "Amsterdam"),
+(7, "00008", "Shonda Leer", "São Paulo");
