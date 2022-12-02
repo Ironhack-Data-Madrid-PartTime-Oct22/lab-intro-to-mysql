78dbe86c074e2037d1de5303ae98c6223707651d
annassanchez <annassanchez@gmail.com>
2022-12-02 21:20:25 +0100

    seeding done (～￣▽￣)～

---
+++ your-code/seeding.sql
@@ -0,0 +1,60 @@
+USE lab_sql_intro;
+
+-- INSERT EN CARS
+INSERT INTO cars
+VALUES (0, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue')
+	, (1, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red')
+    , (2, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White')
+    , (3, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver')
+    , (4, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray')
+    , (5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+
+-- tengo que hacer un update posterior, no me coge como valor el 0 ?    
+UPDATE cars
+SET idCars = 0
+WHERE VIN = '3K096I98581DHSNUP';
+
+SELECT * FROM cars;
+
+-- INSERT EN CUSTOMERS
+INSERT INTO customers
+VALUES (0, 10001, 'Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', 28045)
+	, (1, 20001, 'Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', 33130)
+    , (2, 30001, 'Napoléon Bonaparte', '+33 1 79 75 40 00', '-', ' 	40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', 75008);
+-- lo mismo con esta tabla
+UPDATE customers
+SET idCustomers = 0
+WHERE CustomerID = 10001;
+
+SELECT * FROM customers;
+
+-- INSERT EN CARS
+INSERT INTO salesperson
+VALUES (0, '00001', 'Petey Cruiser', 'Madrid')
+	, (1, '00002', 'Anna Sthesia', 'Barcelona')
+    , (2, '00003', 'Paul Molive', 'Berlin')
+    , (3, '00004', 'Gail Forcewind', 'Paris')
+    , (4, '00005', 'Paige Turner', 'Mimia')
+    , (5, '00006', 'Bob Frapples', 'Mexico City')
+    , (6, '00007', 'Walter Melon', 'Amsterdam')
+    , (7, '00008', 'Shonda Leer', 'São Paulo');
+
+-- el update para el índice 0
+UPDATE salesperson
+SET idSalesperson = 0
+WHERE staffid = '00001';
+
+SELECT * FROM salesperson;
+
+-- INSERT EN CARS
+INSERT INTO invoices
+VALUES (0, '852399038', '2018-08-22', 0, 1, 3)
+	, (1, '731166526', '2018-12-31', 3, 0, 5)
+    , (2, '271135104', '2019-01-22', 2, 2, 7);
+
+-- el update para el índice 0 
+UPDATE invoices
+SET idInvoices = 0
+WHERE invoice_number = '852399038';
+
+SELECT * FROM invoices;
