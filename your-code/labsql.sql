526134b19a7d7c2ada8bcef01c8afae5bbd0ed25
ppastorcarrillo <116591098+ppastorcarrillo@users.noreply.github.com>
2022-12-03 08:14:35 +0100

    Problemas actualizando las tablas, invoice no se actualizan las columnas

--- your-code/labsql.sql
+++ your-code/labsql.sql
@@ -1,7 +1,15 @@
-SELECT * FROM labsql.salesperson;
-USE labsql;
+SELECT * FROM labsql
+INSERT INTO cars
+VALUES 
+(0, "3K096I98581DHSNUP", "Volkswagen", "Tiguan", 2019, "Blue"),
+(1, "ZM8G7BEUQZ97IH46V", "Peugeot", "Rifter", 2019 ,"Red"),
+(2, "RKXVNNIHLVVZOUB4M" , "Ford", "Fusion" , 2018, "White"),
+(3, "HKNDGS7CU31E9Z7JW", "Toyota", "RAV4", 2018, "Silver"),
+(4, "DAM41UDN3CHU2WVF6", "Volvo", "V60", 2019, "Gray"),
+(5, "DAM41UDN3CHU2WVF6", "Volvo", "V60 Cross Country", 2019, "Gray");
 
-INSERT INTO
+
+INSERT INTO salesperson
 VALUES
 (0, "00001", "Petey Cruiser", "Madrid"),
 (1, "00002", "Anna Sthesia", "Barcelona"),
@@ -11,3 +19,21 @@ VALUES
 (5, "00006", "Bob Frapples", "Mexico City"),
 (6, "00007", "Walter Melon", "Amsterdam"),
 (7, "00008", "Shonda Leer", "São Paulo");
+
+update salesperson
+set store = "Miami"
+where idsalesperson = 4;
+
+
+ INSERT INTO customer
+ VALUES
+(0, "10001", "Pablo Picasso", "+34636176382", "ppicasso@gmail.com", "Paseo de la Chopera, 14", "Madrid", "Madrid", "Spain", "28045"),
+(1, "20001", "Abraham Lincoln", "+1 3059077086", "lincoln@us.gov", "120 SW 8th St", "Miami", "Florida", "United States", "33130"),
+(2, "30001", "Napoléon Bonaparte", "+33 179754000", "hello@napoleon.me", "40 Rue du Colisée", "Paris", "Île-de-France", "France", "75008");
+
+
+INSERT INTO invoices
+VALUES
+(0, "852399038",  "2018-08-22", 0, 1, 3),
+(1, "731166526", "2018-12-31", 3, 0, 5),
+(2, "271135104", "2019-01-22", 2, 2, 7);
